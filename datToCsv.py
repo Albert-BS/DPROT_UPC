0fea342112e010d834aab2f4937f1be07ac27185
Albert-BS <albertbertranserrano@gmail.com>
2022-10-01 07:22:31 +0200

    Break done

---
+++ datToCsv.py
@@ -0,0 +1,50 @@
+import os
+import pandas as pd
+
+### Convert .dat files to .csv files ###
+
+files_to_convert = './datFiles'
+csv_dir = './csvFiles'
+
+cd_files = sorted(os.listdir(files_to_convert))
+
+for i in range(len(cd_files)):
+    name = cd_files[i]
+    nameFile = name[:-4]+'.csv'
+    data = pd.read_csv(files_to_convert + '\\' + cd_files[i], sep=' ', header=None)
+    data.dropna(inplace=True)
+    data.to_csv(csv_dir + '\\' + nameFile, index=False)
+
+###-----------------------------------------###
+
+### Merge .csv files from bytes_03FFxx.csv to bytes_0FFF.csv ###
+
+df = pd.concat(map(pd.read_csv, ['./csvFiles/bytes_03FFxx.csv',
+                                 './csvFiles/bytes_04FFxx.csv',
+                                 './csvFiles/bytes_05FFxx.csv',
+                                 './csvFiles/bytes_06FFxx.csv',
+                                 './csvFiles/bytes_07FFxx.csv',
+                                 './csvFiles/bytes_08FFxx.csv',
+                                 './csvFiles/bytes_09FFxx.csv',
+                                 './csvFiles/bytes_0AFFxx.csv',
+                                 './csvFiles/bytes_0BFFxx.csv',
+                                 './csvFiles/bytes_0CFFxx.csv',
+                                 './csvFiles/bytes_0DFFxx.csv',
+                                 './csvFiles/bytes_0EFFxx.csv',
+                                 './csvFiles/bytes_0FFFxx.csv']))
+
+df.to_csv('./csvFiles/filesMerged.csv', index=False)
+
+###-----------------------------------------###
+
+### Add headers to final .csv files ###
+
+key = pd.read_csv('./csvFiles/filesMerged.csv')
+key.rename({'0': 'IV', '1': 'Cypher'}, axis=1, inplace=True)
+key.to_csv('./keyfile.csv', index=False)
+
+message = pd.read_csv('./csvFiles/bytes_01FFxx.csv')
+message.rename({'0': 'IV', '1': 'Cypher'}, axis=1, inplace=True)
+message.to_csv('./messageFile.csv', index=False)
+
+###-----------------------------------------###
---
+++ rc4Breaking.py
@@ -0,0 +1,75 @@
+import csv
+
+# Known values
+
+message = ''
+key = ''
+
+print('Message is -> ' + message + '\nKey is -> ' + key)
+print('===============')
+
+# Initialize frequency vector #
+
+freqHistogram = []
+for i in range(0, 255):
+    freqHistogram.append(0)
+
+# Guessing message #
+with open('./messageFile.csv', mode='r') as file:
+    fileReader = csv.DictReader(file)
+    for row in fileReader:
+        for mValue in range(0, 255):
+            keyStream = mValue ^ int(row['Cypher'], 16)
+            expectedValue = (int(row['IV'][-2:], 16) + 2) % 255
+            if keyStream == expectedValue:
+                freqHistogram[mValue] += 1
+
+maxFreq = max(freqHistogram)
+mesGuess = freqHistogram.index(maxFreq)
+print('m[0]: ' + chr(mesGuess) + ' with freq. ' + str(maxFreq))
+# print(str(chr(mesGuess) == message))  # If we know the message #
+print('===============')
+
+# Guessing key #
+
+keyGuess = []
+iv = 3  # Initial value of the first IV when guessing the key #
+incrementX = 6  # Initial increment using the 'second fact: x + 6 + k[0]', it will increment when changing the IV #
+kPos = 0
+
+with open('./keyfile.csv', mode='r') as file:
+    fileReader = csv.DictReader(file)
+    for row in fileReader:
+        if row['IV'] == ('X0'.join('{:02X}'.format(iv)) + 'FF00'):
+            freqHistogram = []
+            for i in range(0, 255):
+                freqHistogram.append(0)
+
+        for kValue in range(0, 255):
+            keyStream = mesGuess ^ int(row['Cypher'], 16)
+            expectedValue = (int(row['IV'][-2:], 16) + incrementX + kValue) % 255  # Second fact #
+            if keyStream == expectedValue:
+                freqHistogram[kValue] += 1
+
+        if row['IV'] == ('X0'.join('{:02X}'.format(iv)) + 'FFFF'):
+            maxFreq = max(freqHistogram)
+            kGuess = freqHistogram.index(maxFreq)
+            keyGuess.append(kGuess)
+            print('k[' + str(kPos) + ']: 0x' + ''.join('{:02X}'.format(kGuess)) + ' with freq. ' + str(maxFreq))
+            # print(str(kGuess == int(key[kPos * 2: kPos * 2 + 2], 16))) # If we know the key #
+
+            iv += 1
+            incrementX += iv + kGuess
+            kPos += 1
+
+# Final output #
+
+print('===============')
+keyRecovered = ''.join('{:02x}'.format(i) for i in keyGuess)
+if key == '':
+    print('Key guess: ' + keyRecovered)
+elif key == keyRecovered:
+    print('Key recovered: ' + key + '==' + keyRecovered)
+else:
+    print('Key can not be recovered: ' + key + '!=' + keyRecovered)
+print('===============')

fe26461661842513935e503648e2d39298ead476
Albert-BS <albertbertranserrano@gmail.com>
2022-11-07 16:00:30 +0100

    Add file in process

--- 3rd Lab Merkle Trees/addFile.py
+++ 3rd Lab Merkle Trees/addFile.py
@@ -1,60 +1,68 @@
 import os
 
+nodes_path = "./nodes/"
+
 
 def createLayer(layerIndex, nFiles):
     counter = 0
     for j in range(0, nFiles, 2):
         if j == nFiles - 1 and nFiles % 2 == 1:
-            os.system("cat node.pre node" + str(layerIndex) + "." + str(j)
-                      + " | openssl dgst -sha1 -binary | xxd -p > ./nodes/node" + str(layerIndex + 1) + "."
+            os.system("cat node.pre " + nodes_path + "node" + str(layerIndex) + "." + str(j)
+                      + " | openssl dgst -sha1 -binary | xxd -p > " + nodes_path + "node" + str(layerIndex + 1) + "."
                       + str(counter))
         else:
-            os.system("cat node.pre node" + str(layerIndex) + "." + str(j) + " node" + str(layerIndex) + "."
-                      + str(j + 1) + " | openssl dgst -sha1 -binary | xxd -p > ./nodes/node" + str(layerIndex + 1)
-                      + "." + str(counter))
+            os.system(
+                "cat node.pre " + nodes_path + "node" + str(layerIndex) + "." + str(j) + " node" + str(layerIndex) + "."
+                + str(j + 1) + " | openssl dgst -sha1 -binary | xxd -p > " + nodes_path + "node" + str(layerIndex + 1)
+                + "." + str(counter))
 
         os.system("echo -n '" + str(layerIndex + 1) + ":" + str(counter) + ":' >> temp.txt")
-        os.system("cat node" + str(layerIndex + 1) + "." + str(counter) + " >> temp.txt")
+        os.system("cat " + nodes_path + "node" + str(layerIndex + 1) + "." + str(counter) + " >> temp.txt")
         counter = counter + 1
 
 
 doc_path = input("Introduce the full path of the file you want to add:")
 
 f = open("hash_tree.txt", "r")
-public_info = f.readline()
+public_info = f.readline()  # doc.pre + node.pre + root hash
 info = public_info.rsplit(":")  # Getting public info of the hash tree to modify
-num_hashes = int(info[-2])  # Getting the number of nodes of the hash tree
+num_hashes = int(info[-3])  # Getting the number of files composing the Merkle tree
 
 info_nodes = ""
 for i in range(num_hashes):  # Reads the private info of the hash tree
     info_nodes = info_nodes + f.readline()
 f.close()
-os.system("cat doc.pre " + doc_path + " | openssl dgst -sha1 -binary | xxd -p > node0." + str(
+os.system("cat doc.pre " + doc_path + " | openssl dgst -sha1 -binary | xxd -p > " + nodes_path + "node0." + str(
     num_hashes))  # Computes the hash of the new node
 
 f = open("temp.txt", "a")
-f.write(info_nodes)
+f.write(info_nodes)  # Write previous files: 0:0 to 0:num_hashes-1
 f.close()
-os.system("echo -n '" + str(0) + ":" + str(
-    num_hashes) + ":' >> temp.txt")  # Adding the new node to the private part of the hash tree
-os.system("cat node" + str(0) + "." + str(num_hashes) + " >> temp.txt")
-num_hashes = num_hashes + 1
-old_nodes = num_hashes
+os.system("echo -n '" + str(0) + ":" + str(num_hashes) + ":' >> temp.txt")  # Adding the new node
+os.system("cat " + nodes_path + "node" + str(0) + "." + str(num_hashes) + " >> temp.txt")
+# num_hashes = num_hashes + 1
 
+i = 0
+j = num_hashes
+while j > 0:
+    i = i + 1
+    odd = j % 2
+    j = j/2
+    if odd == 1:
+        os.system("cat node.pre " + nodes_path + "node" + str(i-1) + "." + str(2 * j) + " " + nodes_path + "node" + str(i-1) + " " + str(2 * j + 1)) + " | openssl dgst -sha1 -binary |"
+# Modify
 layer = 0
 while num_hashes > 1:  # Determine if it is necessary to create another layer or the root is reached
     createLayer(layer, num_hashes)
     layer = layer + 1
 
     if num_hashes % 2 == 1:
-
         num_hashes = int(num_hashes / 2) + 1
     else:
-
         num_hashes = int(num_hashes / 2)
 
-root_hash = os.popen("cat node" + str(layer) + ".0").read()
-public_info = "MerkleTree:sha1:353535353535:e8e8e8e8e8e8:" + str(old_nodes) + ":" + str(
+root_hash = os.popen("cat " + nodes_path + "node" + str(layer) + ".0").read()
+public_info = "MerkleTree:sha1:3C3C3C3C:F5F5F5F5:" + str(num_hashes) + ":" + str(
     layer + 1) + ":" + root_hash  # Recomputing the public info of the hash tree
 
 os.system("echo -n '" + public_info + "' > hash_tree.txt")
--- 3rd Lab Merkle Trees/buildTree.py
+++ 3rd Lab Merkle Trees/buildTree.py
@@ -1,20 +1,22 @@
 import os
 
 
+nodes_path = "./nodes/"
+
 def createLayer(layerIndex, nFiles):
     counter = 0
     for j in range(0, nFiles, 2):
         if j == nFiles - 1 and nFiles % 2 == 1:
-            os.system("cat node.pre node" + str(layerIndex) + "." + str(j)
-                      + " | openssl dgst -sha1 -binary | xxd -p > ./nodes/node" + str(layerIndex + 1) + "."
+            os.system("cat node.pre " + nodes_path + "node" + str(layerIndex) + "." + str(j)
+                      + " | openssl dgst -sha1 -binary | xxd -p > " + nodes_path + "node" + str(layerIndex + 1) + "."
                       + str(counter))
         else:
-            os.system("cat node.pre node" + str(layerIndex) + "." + str(j) + " node" + str(layerIndex) + "."
-                      + str(j + 1) + " | openssl dgst -sha1 -binary | xxd -p > ./nodes/node" + str(layerIndex + 1)
+            os.system("cat node.pre " + nodes_path + "node" + str(layerIndex) + "." + str(j) + " " + nodes_path + "node" + str(layerIndex) + "."
+                      + str(j + 1) + " | openssl dgst -sha1 -binary | xxd -p > " + nodes_path + "node" + str(layerIndex + 1)
                       + "." + str(counter))
 
         os.system("echo -n '" + str(layerIndex + 1) + ":" + str(counter) + ":' >> temp.txt")
-        os.system("cat node" + str(layerIndex + 1) + "." + str(counter) + " >> temp.txt")
+        os.system("cat " + nodes_path + "node" + str(layerIndex + 1) + "." + str(counter) + " >> temp.txt")
         counter = counter + 1
 
 
@@ -28,14 +30,13 @@ docs_files = os.listdir(docs_path)
 n = len(docs_files)  # Number of files in the "docs" directory
 
 for i in range(n):
-    os.system("cat doc.pre " + docs_path + docs_files[i] + " | openssl dgst -sha1 -binary | xxd -p > ./nodes/node0." + str(i))
+    os.system("cat doc.pre " + docs_path + docs_files[i] + " | openssl dgst -sha1 -binary | xxd -p > " + nodes_path + "node0." + str(i))
     os.system("echo -n '" + str(0) + ":" + str(i) + ":' >> temp.txt")
-    os.system("cat ./nodes/node" + str(0) + "." + str(i) + " >> temp.txt")
+    os.system("cat " + nodes_path + "node" + str(0) + "." + str(i) + " >> temp.txt")
 
 layer = 0
 num_hashes = n
 while num_hashes > 1:  # Determine if it is necessary to create another layer or the root is reached
-
     createLayer(layer, num_hashes)
     layer = layer + 1
 
@@ -44,7 +45,7 @@ while num_hashes > 1:  # Determine if it is necessary to create another layer or
     else:
         num_hashes = int(num_hashes / 2)
 
-root_hash = os.popen("cat ./nodes/node" + str(layer) + ".0").read()
+root_hash = os.popen("cat " + nodes_path + "node" + str(layer) + ".0").read()
 public_info = "MerkleTree:sha1:3C3C3C3C:F5F5F5F5:" + str(n) + ":" + str(layer + 1) + ":" + root_hash
 
 os.system("echo -n '" + public_info + "' > hash_tree.txt")

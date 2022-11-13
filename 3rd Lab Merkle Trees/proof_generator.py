aa31e0d9e7c41206869c6724723cbf583425f9ce
Albert-BS <albertbertranserrano@gmail.com>
2022-11-13 12:28:31 +0100

    Done

--- 3rd Lab Merkle Trees/proof_generator.py
+++ 3rd Lab Merkle Trees/proof_generator.py
@@ -1,37 +1,30 @@
-import math
 import os
 import math
 
 docs_path = "./docs/"
-text=open('hash_tree.txt','r')
-lines=text.readlines()
+text = open('hash_tree.txt', 'r')
+lines = text.readlines()
 
-N=int(lines[0].split(':')[-2]) ## Number of layers
-i=0 
-j=int(input("Position of the document:"))
+n = int(lines[0].split(':')[-2])  # Number of layers
+nDocs = int(lines[0].split(':')[-3])  # Number of documents
 
-os.system( "cat doc.pre "+docs_path+"doc"+str(j)+" | openssl dgst -sha1 -binary | xxd -p > node0."+str(j))
+j = int(input("Position of the document:"))
 
-nodes=[None]*(N-1)
-
-for k in range(N-1):
-    if((j+2)%2 ==0 ):
-        nodes[k]=str(i)+":"+str(j+1)
-    else:
-        nodes[k]=str(i)+":"+str(j-1)
-    i=i+1
-    j=math.trunc(j/2)
-
-print("List of nodes that we need to verify the proof: ",nodes)
-
-i=0
-proof=open('proof.txt','w')
+proof = open('proof.txt', 'w')
 proof.write(lines[0])
-for line in lines:
-    if(i==N-1):
-        break
-    if nodes[i] in line:
-        proof.write(line)
-        i=i+1
-        
 proof.close()
+
+for i in range(n - 1):
+    if (j % 2) == 0:
+        if (j + 1) < nDocs:
+            os.system("echo -n '" + str(i) + ":" + str(j + 1) + ":' >> proof.txt")
+            os.system("cat nodes/node" + str(i) + "." + str(j + 1) + " >> proof.txt")
+        else:
+            os.system("echo -n '" + str(i) + ":" + str(j + 1) + ":\n' >> proof.txt")
+    else:
+        os.system("echo -n '" + str(i) + ":" + str(j - 1) + ":' >> proof.txt")
+        os.system("cat nodes/node" + str(i) + "." + str(j - 1) + " >> proof.txt")
+
+    j = math.trunc(j / 2)
+    i = i + 1
+    nDocs = math.trunc(nDocs / 2 + nDocs % 2)
--- 3rd Lab Merkle Trees/proof_verifier.py
+++ 3rd Lab Merkle Trees/proof_verifier.py
@@ -1,54 +1,56 @@
-import math
 import os
 import math
-import time
-
-## TEST
 
 docs_path = "./docs/"
 
-i=0
-j=int(input("Position of the document:"))
-os.system( "cat doc.pre "+docs_path+"doc"+str(j)+" | openssl dgst -sha1 -binary | xxd -p > node0."+str(j))
+i = 0
+j = int(input("Position of the document:"))
 
-proof=open('proof.txt','r')
-lines=proof.readlines()
+proof = open('proof.txt', 'r')
+
+public_info = proof.readline()
+info = public_info.split(':')
+
+alg = info[-6]
+end_node = info[-1]
+
+os.system(
+    "cat doc.pre " + docs_path + "doc" + str(j) + ".dat | openssl dgst -" + alg + " -binary | xxd -p > aux_hash.txt")
+
+lines = proof.readlines()
+
 for line in lines:
-    if("MerkleTree" in line):
-        end_node=line.split(':')[-1] ## Root real hash value
+    os.system(
+        "cp aux_hash.txt aux_hash1.txt")  # We need to create a copy since in the last command we need a secondary file to compute the new hash or it will try to open two times the same file, one for reading and one for writing
+    next_node = ((line.split(":"))[-1]).split('\n')[0]
+    os.system("echo '" + next_node + "' > next_node.txt")
+
+    # Constructing the nodes command in the good order
+
+    if line[4] == "\n":  # Check if the node exists or not
+        nodes_c = "aux_hash1.txt"
     else:
-        next_node=((line.split(":"))[-1]).split('\n')[0]
-        print('Next node: ',next_node)
-
-        command="echo '"+next_node+"' >node"+line.split(":")[0]+"."+line.split(":")[1]
-        #print(command)
-        os.system(command)
-
-        ## Constructing the nodes command in the good order
-
-        if((int(line.split(":")[1]))>j):
-            nodes_c= "node"+str(i)+"."+str(j)+" node"+line.split(":")[0]+"."+line.split(":")[1]
+        if (int(line.split(":")[1])) > j:
+            nodes_c = "aux_hash1.txt next_node.txt"
         else:
-            nodes_c= "node"+line.split(":")[0]+"."+line.split(":")[1]+" node"+str(i)+"."+str(j)
-            
+            nodes_c = "next_node.txt aux_hash1.txt"
 
-        print("Nodes used to calculate the following command: ",nodes_c)
-        
-        ## indexes of the following node
+    # indexes of the following node
 
-        j=math.trunc(j/2)
-        i=i+1
+    j = math.trunc(j / 2)
+    i = i + 1
 
-        ## Generating the following node
+    # Generating the following node
 
-        os.popen("cat node.pre "+nodes_c+" | openssl dgst -sha1 -binary | xxd -p > node"+str(i)+"."+str(j))
-        time.sleep(2)
-        print('>> node'+str(i)+"."+str(j),os.popen('cat node'+str(i)+"."+str(j)).read())
+    os.system("cat node.pre " + nodes_c + " | openssl dgst -" + alg + " -binary | xxd -p > aux_hash.txt")
 
+# Verify if the node calculated with the proof.txt file is the same that the root real has value
 
-## Verify if the node calculated with the proof.txt file is the same that the root real has value
+last_node = os.popen("cat aux_hash.txt").read()
 
-last_node=os.popen('cat node'+str(i)+"."+str(j)).read()
-
-if(last_node==end_node):
+if last_node == end_node:
     print("OK, proof verified")
+else:
+    print("Wrong, not verified")
+
+os.system("rm aux_hash.txt aux_hash1.txt next_node.txt")
